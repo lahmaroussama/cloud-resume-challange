49ab376a76e15273dc5d99c55d46be6a5fa781b7
lahmaroussama <lahmar.oussama01@gmail.com>
2023-11-01 14:09:47 +0100

    update lambda

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -262,7 +262,7 @@
   const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://egdvvkdspsmnfnxzmxogakky2i0uwkjf.lambda-url.eu-north-1.on.aws/"
+        "https://xtrrwqkv3p5qbjln6hfmri5zue0qelpj.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
